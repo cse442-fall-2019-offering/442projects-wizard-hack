17e8c6408620bee5167eed2c4984b5a4392f4ea2
Melissa Tillinghast <missy.haberer@gmail.com>
2019-10-05 16:24:11 -0400

    Added a player health status bar to the screen with a heart icon.

---
+++ Wizard Hack/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int initialHealth = 100;
+    public int enemyCurrentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* enemy initial health should be 100 when starting */
+        enemyCurrentHealth = initialHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void damageEnemy(int damageAmount)
+    {
+        enemyCurrentHealth -= damageAmount;
+    }
+}
---
+++ Wizard Hack/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int initialHealth = 100;
+    public int playerCurrentHealth;
+    public int initialMana = 100;
+    public int playerCurrentMana;
+    public Slider playerHealthSlider;
+    public Slider playerManaSlider;
+    public Image damageImage;
+
+    bool playerDead;
+    bool playerDamaged;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* the current health of the player should be the initial value when starting game*/
+        playerCurrentHealth = initialHealth;
+        playerCurrentMana = initialMana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void damagePlayer(int damageAmount)
+    {
+        playerCurrentHealth -= damageAmount;
+        playerHealthSlider.value = playerCurrentHealth;
+    }
+
+    public void healPlayer(int healingAmount)
+    {
+        playerCurrentHealth += healingAmount;
+        playerHealthSlider.value = playerCurrentHealth;
+    }
+
+    public void useMana(int usedAmount)
+    {
+        playerCurrentMana -= usedAmount;
+        playerManaSlider.value = playerCurrentMana;
+    }
+
+    public void gainMana(int gainAmount)
+    {
+        playerCurrentMana += gainAmount;
+        playerManaSlider.value = playerCurrentMana;
+    }
+
+}
