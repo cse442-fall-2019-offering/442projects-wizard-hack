e61c76c663ff16af9a4ab9a6ba2f1da3a61dc78f
Mckayla Brefka <mckaylab@buffalo.edu>
2019-10-09 12:55:27 -0400

    Player Movement Animation
    Animations have been added to show animation of movement when the player moves down, up, left, right
    
    This commit is linked to task # 66

--- Wizard Hack/Assets/Scripts/PlayerMovement.cs
+++ Wizard Hack/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -15,6 +16,10 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x=Input.GetAxisRaw("Horizontal");
         movement.y= Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate(){
