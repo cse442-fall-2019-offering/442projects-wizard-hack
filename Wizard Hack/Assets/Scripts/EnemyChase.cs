aeb1c754a0d5985b81ec210d8b867604f8037050
jmekjean <jmekjean@buffalo.edu>
2019-10-15 03:10:30 -0400

    Enemy Movement Animation
    Task #79

--- Wizard Hack/Assets/Scripts/EnemyChase.cs
+++ Wizard Hack/Assets/Scripts/EnemyChase.cs
@@ -41,11 +41,7 @@ public class EnemyChase : MonoBehaviour
         	animator.SetFloat("Speed", 0);
         }
 
-        if (distanceToPlayer <= 2){
-        	animator.SetBool("Should_Attack", true);
-        } else {
-        	animator.SetBool("Should_Attack", false);
-        }
+        
         // ** End **
     }
 
