93f4abf92df0d4be252e4668b95f3071e1e28be3
jmekjean <jmekjean@buffalo.edu>
2019-10-15 03:06:24 -0400

    Added Enemy Movement Animations
    Task #79

--- Wizard Hack/Assets/Scripts/EnemyChase.cs
+++ Wizard Hack/Assets/Scripts/EnemyChase.cs
@@ -6,24 +6,20 @@ public class EnemyChase : MonoBehaviour
 {
 	public float speed;
 	public float distanceToStartAttack;
+	public Animator animator;
+    public float x;
+    public float y;
+    public float z;
 
 	private Transform player;
-	private float x;
-	private float y;
-	private float z;
     // Start is called before the first frame update
-    void Start()
-    {
-        x = 5;
-        y = 5;
-        z = 5;
+    void Start() {
         transform.localScale = new Vector3(x, y, z);
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
     	float distanceToPlayer = Vector2.Distance(transform.position, player.position);
         if(distanceToPlayer < distanceToStartAttack && distanceToPlayer > 1) {
 	    	// ** LOOK AT PLAYER **
@@ -37,9 +33,18 @@ public class EnemyChase : MonoBehaviour
 	        // ** End **
 
         	// ** CHASE PLAYER **
-
             	transform.position = Vector2.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
+            	animator.SetFloat("Speed", 1);
+            // ** CATCH PLAYER ** 
 
+        } else {
+        	animator.SetFloat("Speed", 0);
+        }
+
+        if (distanceToPlayer <= 2){
+        	animator.SetBool("Should_Attack", true);
+        } else {
+        	animator.SetBool("Should_Attack", false);
         }
         // ** End **
     }
