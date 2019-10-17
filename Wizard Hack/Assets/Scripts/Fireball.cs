957d3b7ddd72594ba606805c30b7d6f95ef741bb
James Morrow <jmorrow2@buffalo.edu>
2019-10-16 21:13:04 -0400

    created fireball prefab
    created the prefab for the fireball. The fireball prefab has attributes such as speed, damage, anad animation.
    
    this is for task #80

---
+++ Wizard Hack/Assets/Scripts/Fireball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public int damage = 20;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
+        if(enemyHealth!=null)
+        {
+            enemyHealth.damageEnemy(damage);   
+        }
+        Destroy(gameObject);
+    }
+}
